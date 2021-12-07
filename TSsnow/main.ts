b3707cbe512ca5c22e8e854f6ab29c1a74059e4a
David Osborne <david.osborne@outlook.com>
2021-12-07 11:25:50 -0500

    Add files via upload

--- TSsnow/main.ts
+++ TSsnow/main.ts
@@ -1,13 +1,13 @@
-let gbl_canvasWidth = window.innerWidth,
-    gbl_canvasHeight = window.innerHeight,
-    cvs,
-    ctx,
-    secondsPassed,
-    oldTimeStamp,
-    fps = 0,
-    gbl_particleCount = 0,
+let gbl_canvasWidth:number = window.innerWidth,
+    gbl_canvasHeight:number = window.innerHeight,
+    cvs:any,
+    ctx:any,
+    secondsPassed:number,
+    oldTimeStamp:number,
+    fps:number = 0,
+    gbl_particleCount:number = 0,
     gbl_timestampStart: Date,
-    flakes = [];
+    flakes:any[] = [];
 
 window.onload = init;
 
@@ -16,12 +16,10 @@ function init() {
     cvs = document.getElementById('canvas');
     ctx = cvs.getContext('2d');
 
-    //scaleCanvas();
-
-    const startTime = new Date;
+    const startTime:Date = new Date;
     gbl_timestampStart = startTime;
 
-    createEventListeners();
+    //createEventListeners();
 
     // Start the first frame request
     window.requestAnimationFrame(gameLoop);
@@ -31,33 +29,12 @@ function createEventListeners() {
     window.addEventListener('resize', windowSize);
 }
 
-function scaleCanvas() {
-    var scaleFactor = backingScale(ctx);
-
-    if (scaleFactor > 1) {
-        ctx.canvas.width = ctx.canvas.width * scaleFactor;
-        ctx.canvas.height = ctx.canvas.height * scaleFactor
-        // update the context for the new canvas scale
-        ctx = cvs.getContext("2d");
-    }
-}
-
 function windowSize() {
     gbl_canvasWidth = window.innerWidth;
     gbl_canvasHeight = window.innerHeight;
 
-    //scaleCanvas();
     ctx.canvas.width = gbl_canvasWidth;
     ctx.canvas.height = gbl_canvasHeight;
-}
-
-function backingScale(context) {
-    if ('devicePixelRatio' in window) {
-        if (window.devicePixelRatio > 1) {
-            return window.devicePixelRatio;
-        }
-    }
-    return 1;
 }
 
 function gameLoop(timeStamp) {
@@ -72,7 +49,7 @@ function gameLoop(timeStamp) {
     generateArrays();
     draw();
     drawText(fps);
-    iterateArrays();
+    //iterateArrays();
     cleanArrays();
 
     // Keep requesting new frames
@@ -80,8 +57,8 @@ function gameLoop(timeStamp) {
 }
 
 function generateCanvas() {
-    const body = document.getElementById('body');
-    const canvas = document.createElement('canvas');
+    const body:HTMLElement = document.getElementById('body');
+    const canvas:HTMLCanvasElement = document.createElement('canvas');
     canvas.id = 'canvas';
     canvas.width = gbl_canvasWidth;
     canvas.height = gbl_canvasHeight;
@@ -89,28 +66,12 @@ function generateCanvas() {
 }
 
 function clearCanvas() {
-    //clear canvas
     ctx.clearRect(0, 0, gbl_canvasWidth, gbl_canvasHeight);
-    //ctx.fillStyle = 'black';
-    //ctx.fillRect(0, 0, gbl_canvasWidth, gbl_canvasHeight);
-}
-
-function generateArrays() {
-    let randomColor = '#' + Math.random().toString(16).substr(2, 6);
-
-    flakes.push({
-        color: randomColor,
-        angle: randomInt(0, 360),
-        xpos: randomInt(0, gbl_canvasWidth),
-        ypos: 10,
-        radius: randomInt(2, 4),
-        opacity: Math.random() + 0.3
-    });
 }
 
 function iterateArrays() {
     for (let i = 0; i < flakes.length; i++) {
-        var flake = flakes[i];
+        var flake:any = flakes[i];
         if (
             flake.ypos < gbl_canvasHeight
         ) {
@@ -119,29 +80,49 @@ function iterateArrays() {
     }
 }
 
-function cleanArrays() {
-    for (let i = 0; i < flakes.length; i++) {
-        var flake = flakes[i];
-        if (
-            flake.ypos >= gbl_canvasHeight - flake.radius
-            ||
-            flake.ypos < 0 + flake.radius
-            ||
-            flake.xpos > gbl_canvasWidth + flake.radius
-            ||
-            flake.xpos < 0 + flake.radius
-            ||
-            flake.radius == 1
-        ) {
-            flakes.splice(i, 1);
-        }
-    }
+function generateArrays() {
+    let randomColor:string = '#' + Math.random().toString(16).substr(2, 6),
+        speedcalc:number = (Math.random() * 1) + 0.5;
+
+    flakes.push({
+        color: randomColor,
+        xpos: randomInt(0, gbl_canvasWidth),
+        ypos: randomInt(0, 100),
+        radius: randomInt(2, 4),
+        opacity: Math.random() + 0.3, //math random generates between 0 and 1, sets min at 0.3
+        speed: speedcalc,
+        velY: speedcalc,
+        velX: 0,
+        stepSize: (Math.random()) / 30,
+        step: 0
+    });
 }
 
 function draw() {
     //draw particle for each array item
     for (let i = 0; i < flakes.length; i++) {
-        var flake = flakes[i];
+        var flake = flakes[i], //get the flake from flakes
+            minDist = 150,
+            x2 = flake.xpos,
+            y2 = flake.ypos,
+            //dist = Math.sqrt((x2 - x) * (x2 - x) + (y2 - y) * (y2 - y)),
+            dist = Math.sqrt((x2) * (x2) + (y2) * (y2));
+
+        if (dist < minDist) {
+            var force = minDist / (dist * dist),
+                xcomp = flake.xpos / dist,
+                ycomp = flake.ypos / dist,
+                deltaV = force / 2;
+
+            flake.velX -= deltaV * xcomp;
+            flake.velY -= deltaV * ycomp;
+        }
+        else {
+            flake.velX *= .98;
+            if (flake.velY <= flake.speed)
+                flake.velY = flake.speed;
+            flake.velX += Math.cos(flake.step += .05) * flake.stepSize;
+        }
 
         //set fill color
         ctx.fillStyle = "rgba(255,255,255," + flake.opacity + ")";
@@ -150,6 +131,24 @@ function draw() {
         ctx.beginPath();
         ctx.arc(flake.xpos, flake.ypos, flake.radius, 0, 360);
         ctx.fill();
+
+        flake.xpos += flake.velX;
+        flake.ypos += flake.velY;
+    }
+}
+
+function cleanArrays() {
+    for (let i = 0; i < flakes.length; i++) {
+        var flake:any = flakes[i];
+        if (
+            flake.ypos >= gbl_canvasHeight - flake.radius
+            ||
+            flake.xpos > gbl_canvasWidth + flake.radius
+            //||
+            //flake.xpos < 0 - flake.radius
+        ) {
+            flakes.splice(i, 1);
+        }
     }
 }
 
@@ -160,26 +159,14 @@ function drawText(fps: number) {
     let ss = d.getSeconds().toString();
     let ms = d.getMilliseconds().toString();
 
-    ctx.fillStyle = 'dimgray';
-    ctx.fillRect(0, 0, 280, 130);
-
     ctx.font = '24px Courier New';
     ctx.fillStyle = 'orange';
-    ctx.fillText('FPS: ' + fps, 10, 22);
+    ctx.fillText('FPS: ' + fps, 10, gbl_canvasHeight-40);
     ctx.font = '15px Courier New';
     ctx.fillStyle = 'lime';
-    ctx.fillText('Timestamp: ' + hh + ':' + mm + ':' + ss + '.' + ms, 10, 40);
-    ctx.fillText('Particle count: ' + flakes.length.toString(), 10, 80);
-    ctx.fillText('Elapsed time: ' + timeDiff().toString(), 10, 100);
-    ctx.fillText('Window size: ' + gbl_canvasWidth + 'W x ' + gbl_canvasHeight + 'H', 10, 120);
+    ctx.fillText('Particles: ' + flakes.length.toString(), 10, gbl_canvasHeight-20);
 }
 
 function randomInt(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
-}
-
-function timeDiff() {
-    var timeDiff = new Date().getTime() - gbl_timestampStart.getTime();
-    timeDiff /= 1000; //strip ms
-    return timeDiff;
 }
--- TSsnow/out/main.js
+++ TSsnow/out/main.js
@@ -1,58 +1,23 @@
-var gbl_canvasWidth = window.innerWidth, gbl_canvasHeight = window.innerHeight, cvs, ctx, secondsPassed, oldTimeStamp, fps = 0, gbl_particleCount = 0, gbl_timestampStart, 
-/*
-gbl_listX: number[] = [], // x pos
-gbl_listY: number[] = [], // y pos
-gbl_listR: number[] = [], // radius
-gbl_listC: string[] = [], // color
-gbl_listA: number[] = [], // angle
-gbl_listT: number[] = [], // type
-*/
-flakes = [];
+var gbl_canvasWidth = window.innerWidth, gbl_canvasHeight = window.innerHeight, cvs, ctx, secondsPassed, oldTimeStamp, fps = 0, gbl_particleCount = 0, gbl_timestampStart, flakes = [];
 window.onload = init;
 function init() {
     generateCanvas();
     cvs = document.getElementById('canvas');
     ctx = cvs.getContext('2d');
-    //scaleCanvas();
     var startTime = new Date;
     gbl_timestampStart = startTime;
-    createEventListeners();
+    //createEventListeners();
     // Start the first frame request
     window.requestAnimationFrame(gameLoop);
 }
 function createEventListeners() {
     window.addEventListener('resize', windowSize);
 }
-function scaleCanvas() {
-    var scaleFactor = backingScale(ctx);
-    if (scaleFactor > 1) {
-        ctx.canvas.width = ctx.canvas.width * scaleFactor;
-        ctx.canvas.height = ctx.canvas.height * scaleFactor;
-        // update the context for the new canvas scale
-        ctx = cvs.getContext("2d");
-    }
-}
 function windowSize() {
     gbl_canvasWidth = window.innerWidth;
     gbl_canvasHeight = window.innerHeight;
-    //scaleCanvas();
     ctx.canvas.width = gbl_canvasWidth;
     ctx.canvas.height = gbl_canvasHeight;
-    /*
-        gbl_listX = [];
-        gbl_listY = [];
-        gbl_listA = [];
-        gbl_listR = [];
-        gbl_listC = [];
-        */
-}
-function backingScale(context) {
-    if ('devicePixelRatio' in window) {
-        if (window.devicePixelRatio > 1) {
-            return window.devicePixelRatio;
-        }
-    }
-    return 1;
 }
 function gameLoop(timeStamp) {
     // Calculate the number of seconds passed since the last frame
@@ -64,7 +29,7 @@ function gameLoop(timeStamp) {
     generateArrays();
     draw();
     drawText(fps);
-    iterateArrays();
+    //iterateArrays();
     cleanArrays();
     // Keep requesting new frames
     window.requestAnimationFrame(gameLoop);
@@ -78,29 +43,7 @@ function generateCanvas() {
     body.appendChild(canvas);
 }
 function clearCanvas() {
-    //clear canvas
     ctx.clearRect(0, 0, gbl_canvasWidth, gbl_canvasHeight);
-    //ctx.fillStyle = 'black';
-    //ctx.fillRect(0, 0, gbl_canvasWidth, gbl_canvasHeight);
-}
-function generateArrays() {
-    var randomColor = '#' + Math.random().toString(16).substr(2, 6);
-    flakes.push({
-        color: randomColor,
-        angle: randomInt(0, 360),
-        xpos: randomInt(0, gbl_canvasWidth),
-        ypos: 10,
-        radius: randomInt(2, 4),
-        opacity: Math.random() + 0.3
-    });
-    /*
-    gbl_listC.push(randomColor);
-    gbl_listA.push(randomInt(0, 360));
-    gbl_listX.push(randomInt(0, gbl_canvasWidth));
-    gbl_listY.push(10);
-    gbl_listR.push(randomInt(2, 4));
-    gbl_listT.push(Math.random()+0.3);
-    */
 }
 function iterateArrays() {
     for (var i = 0; i < flakes.length; i++) {
@@ -110,32 +53,59 @@ function iterateArrays() {
         }
     }
 }
-function cleanArrays() {
-    for (var i = 0; i < flakes.length; i++) {
-        var flake = flakes[i];
-        if (flake.ypos >= gbl_canvasHeight - flake.radius
-            ||
-                flake.ypos < 0 + flake.radius
-            ||
-                flake.xpos > gbl_canvasWidth + flake.radius
-            ||
-                flake.xpos < 0 + flake.radius
-            ||
-                flake.radius == 1) {
-            flakes.splice(i, 1);
-        }
-    }
+function generateArrays() {
+    var randomColor = '#' + Math.random().toString(16).substr(2, 6), speedcalc = (Math.random() * 1) + 0.5;
+    flakes.push({
+        color: randomColor,
+        xpos: randomInt(0, gbl_canvasWidth),
+        ypos: randomInt(0, 100),
+        radius: randomInt(2, 4),
+        opacity: Math.random() + 0.3,
+        speed: speedcalc,
+        velY: speedcalc,
+        velX: 0,
+        stepSize: (Math.random()) / 30,
+        step: 0
+    });
 }
 function draw() {
     //draw particle for each array item
     for (var i = 0; i < flakes.length; i++) {
-        var flake = flakes[i];
+        var flake = flakes[i], //get the flake from flakes
+        minDist = 150, x2 = flake.xpos, y2 = flake.ypos, 
+        //dist = Math.sqrt((x2 - x) * (x2 - x) + (y2 - y) * (y2 - y)),
+        dist = Math.sqrt((x2) * (x2) + (y2) * (y2));
+        if (dist < minDist) {
+            var force = minDist / (dist * dist), xcomp = flake.xpos / dist, ycomp = flake.ypos / dist, deltaV = force / 2;
+            flake.velX -= deltaV * xcomp;
+            flake.velY -= deltaV * ycomp;
+        }
+        else {
+            flake.velX *= .98;
+            if (flake.velY <= flake.speed)
+                flake.velY = flake.speed;
+            flake.velX += Math.cos(flake.step += .05) * flake.stepSize;
+        }
         //set fill color
         ctx.fillStyle = "rgba(255,255,255," + flake.opacity + ")";
         //draw filled circle
         ctx.beginPath();
         ctx.arc(flake.xpos, flake.ypos, flake.radius, 0, 360);
         ctx.fill();
+        flake.xpos += flake.velX;
+        flake.ypos += flake.velY;
+    }
+}
+function cleanArrays() {
+    for (var i = 0; i < flakes.length; i++) {
+        var flake = flakes[i];
+        if (flake.ypos >= gbl_canvasHeight - flake.radius
+            ||
+                flake.xpos > gbl_canvasWidth + flake.radius
+            ||
+                flake.xpos < 0 - flake.radius) {
+            flakes.splice(i, 1);
+        }
     }
 }
 function drawText(fps) {
@@ -144,24 +114,14 @@ function drawText(fps) {
     var mm = d.getMinutes().toString();
     var ss = d.getSeconds().toString();
     var ms = d.getMilliseconds().toString();
-    ctx.fillStyle = 'dimgray';
-    ctx.fillRect(0, 0, 280, 130);
     ctx.font = '24px Courier New';
     ctx.fillStyle = 'orange';
-    ctx.fillText('FPS: ' + fps, 10, 22);
+    ctx.fillText('FPS: ' + fps, 10, gbl_canvasHeight - 40);
     ctx.font = '15px Courier New';
     ctx.fillStyle = 'lime';
-    ctx.fillText('Timestamp: ' + hh + ':' + mm + ':' + ss + '.' + ms, 10, 40);
-    ctx.fillText('Particle count: ' + flakes.length.toString(), 10, 80);
-    ctx.fillText('Elapsed time: ' + timeDiff().toString(), 10, 100);
-    ctx.fillText('Window size: ' + gbl_canvasWidth + 'W x ' + gbl_canvasHeight + 'H', 10, 120);
+    ctx.fillText('Particles: ' + flakes.length.toString(), 10, gbl_canvasHeight - 20);
 }
 function randomInt(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
-}
-function timeDiff() {
-    var timeDiff = new Date().getTime() - gbl_timestampStart.getTime();
-    timeDiff /= 1000; //strip ms
-    return timeDiff;
 }
 //# sourceMappingURL=main.js.map
